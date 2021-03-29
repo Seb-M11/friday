a2848f65f35cdcc5716c7d93aa9d6b3f55020642
Seb-M11 <61061771+Seb-M11@users.noreply.github.com>
2021-03-29 15:16:39 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,45 +1,26 @@
-from kivymd.app import MDApp
-from kivy.lang import Builder
-
-
-class Test(MDApp):
+import kivy
+from kivy.app import App
+from kivy.uix.label import Label
+  
+  
+# Replace this with your 
+# current version
+kivy.require('1.11.1')  
+  
+# Defining a class
+class MyFirstKivyApp(App):
+      
+    # Function that returns 
+    # the root widget
     def build(self):
-        self.title = 'Programming'
-        self.theme_cls.primary_palette = "Blue"
-        return Builder.load_string(
-            '''
-BoxLayout:
-    orientation:'vertical'
-    MDToolbar:
-        title: 'Programming'
-        md_bg_color: app.theme_cls.primary_color
-        specific_text_color: 1, 1, 1, 1
-    MDBottomNavigation:
-
-        MDBottomNavigationItem:
-            name: 'screen 1'
-            text: 'Python'
-            icon: 'language-python'
-            Image:
-                id: imageView
-                source: 'python.png'
-
-        MDBottomNavigationItem:
-            name: 'screen 2'
-            text: 'C++'
-            icon: 'language-cpp'
-            Image:
-                id: imageView
-                source: 'cp.png'
-        MDBottomNavigationItem:
-            name: 'screen 3'
-            text: 'Kivy'
-            icon: 'android'
-            Image:
-                id: imageView
-                source: 'kivy.png'
-'''
-        )
-
-
-Test().run()
+          
+        # Label with text Hello World is 
+        # returned as root widget
+        return Label(text ="Hello World !")          
+  
+  
+# Here our class is initialized
+# and its run() method is called. 
+# This initializes and starts 
+# our Kivy application.
+MyFirstKivyApp().run()  
